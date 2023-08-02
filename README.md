ee7767d8cffa46e6e1fca78fccd0dd2041779cbd
Wikaobl <wikob@icloud.com>
2023-08-02 12:34:39 +0200

    description and usage of the WeatherApp

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # WeatherApp
+
 <img src="https://github.com/Wikaobl/WeatherApp/assets/107032701/a8dcae79-a69d-48d8-9a11-1ca44bee1361" alt="screenshot of the website" width="70%">
+
+## Desription
+
+This page displays weather information such as temperature and humidity for a selected city. You can choose the city yourself. The pictures change depending on the weather (sunny, cloudy, stormy etc.). In case you enter a city that does not exist, the error “**Please enter a valid city name!**” pops out. To create this website I used **_JS, HTML and CSS_**, along with the **_Open Weather API_**.
+
+## Usage
+
+First, run the development server:
+
+```bash
+git clone https://github.com/Wikaobl/WeatherApp
+```
+
+Next, install dependencies
+
+```bash
+cd WeatherApp
+npm install
+```
+
+Run the project
+
+```bash
+npm run
+```
+
+After successfully starting the project, open your web browser and go to the specified URL http://localhost:3000
+
+### Enjoy!
--- script.js
+++ script.js
@@ -1,68 +1,69 @@
-const input = document.querySelector('input')
-const button = document.querySelector('button')
-const cityName = document.querySelector('.city-name')
-const warning = document.querySelector('.warning')
-const photo = document.querySelector('.photo')
-const weather = document.querySelector('.weather')
-const temperature = document.querySelector('.temperature')
-const humidity = document.querySelector('.humidity')
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const cityName = document.querySelector(".city-name");
+const warning = document.querySelector(".warning");
+const photo = document.querySelector(".photo");
+const weather = document.querySelector(".weather");
+const temperature = document.querySelector(".temperature");
+const humidity = document.querySelector(".humidity");
 
+const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q="
-
-const API_KEY = "&appid=0be4cff37606c870e3ab033d0b41c127"
-const API_UNITS = '&units=metric'
+const API_KEY = "&appid=0be4cff37606c870e3ab033d0b41c127";
+const API_UNITS = "&units=metric";
 
 const getWeather = () => {
-    const city = input.value || 'London';
-    const URL = API_LINK + city + API_KEY + API_UNITS;
+  const city = input.value || "London";
+  const URL = API_LINK + city + API_KEY + API_UNITS;
 
-    axios.get(URL).then(res => {
-        console.log(res.data);
-        const temp = res.data.main.temp
-        const hum = res.data.main.humidity
-        const status = Object.assign({}, ...res.data.weather)
+  axios
+    .get(URL)
+    .then((res) => {
+      console.log(res.data);
+      const temp = res.data.main.temp;
+      const hum = res.data.main.humidity;
+      const status = Object.assign({}, ...res.data.weather);
 
-        console.log(status.id);
+      console.log(status.id);
 
-        photo.setAttribute('src', './img/ice.png')
+      photo.setAttribute("src", "./img/ice.png");
 
-        cityName.textContent = res.data.name;
-        temperature.textContent = Math.floor(temp) + "℃"
-        humidity.textContent = hum + '%';
-        weather.textContent = status.main;
+      cityName.textContent = res.data.name;
+      temperature.textContent = Math.floor(temp) + "℃";
+      humidity.textContent = hum + "%";
+      weather.textContent = status.main;
 
-        warning.textContent = '';
-        input.value = ''
-        // console.log(res.data.weather[0].id);
+      warning.textContent = "";
+      input.value = "";
+      // console.log(res.data.weather[0].id);
 
-        if (status.id >= 200 && status.id < 300) {
-            photo.setAttribute('src', './img/thunderstorm.png')
-        } else if (status.id >= 300 && status.id < 400) {
-            photo.setAttribute('src', './img/drizzle.png')
-        } else if (status.id >= 500 && status.id < 600) {
-            photo.setAttribute('src', './img/rain.png')
-        } else if (status.id >= 600 && status.id < 700) {
-            photo.setAttribute('src', './img/ice.png')
-        } else if (status.id >= 700 && status.id < 800) {
-            photo.setAttribute('src', './img/fog.png')
-        } else if (status.id === 800) {
-            photo.setAttribute('src', './img/sun.png')
-        } else if (status.id > 800 && status.id < 900) {
-            photo.setAttribute('src', './img/cloud.png')
-        } else {
-            photo.setAttribute('src', './img/unknown.png')
-
-        }
-    }).catch(() => warning.textContent = 'Wpisz poprawną nazwę miasta!')
-}
+      if (status.id >= 200 && status.id < 300) {
+        photo.setAttribute("src", "./img/thunderstorm.png");
+      } else if (status.id >= 300 && status.id < 400) {
+        photo.setAttribute("src", "./img/drizzle.png");
+      } else if (status.id >= 500 && status.id < 600) {
+        photo.setAttribute("src", "./img/rain.png");
+      } else if (status.id >= 600 && status.id < 700) {
+        photo.setAttribute("src", "./img/ice.png");
+      } else if (status.id >= 700 && status.id < 800) {
+        photo.setAttribute("src", "./img/fog.png");
+      } else if (status.id === 800) {
+        photo.setAttribute("src", "./img/sun.png");
+      } else if (status.id > 800 && status.id < 900) {
+        photo.setAttribute("src", "./img/cloud.png");
+      } else {
+        photo.setAttribute("src", "./img/unknown.png");
+      }
+    })
+    .catch(() => (warning.textContent = "Please enter a valid city name!"));
+};
 
 const enterCheck = (e) => {
-    if (e.key === 'Enter') {
-        getWeather()
-    }
-}
+  if (e.key === "Enter") {
+    getWeather();
+  }
+};
 
-input.addEventListener('keyup', enterCheck);
-button.addEventListener('click', getWeather);
+input.addEventListener("keyup", enterCheck);
+button.addEventListener("click", getWeather);
 getWeather();
